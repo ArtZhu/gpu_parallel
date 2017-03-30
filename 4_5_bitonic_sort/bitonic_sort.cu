89f97072a66393061211a2f20181ad303f83456d
Tiane Zhu <tiane.zhu@gmail.com>
2017-03-29 22:57:55 -0400

    Alg 4_5 source file created

---
+++ 4_5_bitonic_sort/bitonic_sort.cu
@@ -0,0 +1,10 @@
+/* file : bitonic_merge_sort.cu
+ * author : Tiane Zhu
+ * date : Mar 29, 2017
+ *
+ * this program is an implementation of the parallel search algorithm
+ * 	ALGORITHM 4.5 in 
+ * "An Introduction to Parallel Algorithms" - by Joseph Jaja
+ *		p146 - ISBN 9-789201-548563
+ */
+
